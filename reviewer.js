1102cb51dc011b3bbe73554bf36ccf6e9dbbcbd0
mengyuan <3294023022@qq.com>
2026-09-25 16:32:25 +0800

    feat: add DOM hover highlight

--- reviewer.js
+++ reviewer.js
@@ -1,2 +1,37 @@
+// ================================
 // AI HTML Prototype Reviewer
-// Reviewer logic will start from Day 2.
+// Day 2 - DOM Hover Highlight
+// ================================
+
+
+// 鼠标进入某个 DOM 元素时
+document.addEventListener(
+  "mouseover",
+  function (event) {
+
+    const element = event.target;
+
+    element.classList.add(
+      "ai-review-hover"
+    );
+
+    console.log(
+      "Hover DOM:",
+      element
+    );
+  }
+);
+
+
+// 鼠标离开某个 DOM 元素时
+document.addEventListener(
+  "mouseout",
+  function (event) {
+
+    const element = event.target;
+
+    element.classList.remove(
+      "ai-review-hover"
+    );
+  }
+);
